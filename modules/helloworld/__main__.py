7beac630405429497014bb1b2c11a766f92d04f3
quangminh <minhsaco99@gmail.com>
2023-04-18 14:02:39 +0700

    test python

---
+++ modules/helloworld/__main__.py
@@ -0,0 +1,6 @@
+class HelloWorld:
+    def __init__(self) -> None:
+        print('Hello World from HelloWorld')
+
+
+test = HelloWorld()
---
+++ modules/test.py
@@ -0,0 +1,3 @@
+class HelloWorld:
+    def __init__(self) -> None:
+        print('Hello World from modules')
